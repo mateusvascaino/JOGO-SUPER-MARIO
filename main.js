c1180f0f23325b10a8a6733dde8caa10b681c4f9
mateusvascaino <163007807+mateusvascaino@users.noreply.github.com>
2024-09-24 08:44:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const startButton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
 const scoreElement = document.getElementById("score-value")
 
-const audioStart = new Audio("./sound/sound_theme.mp3");
+const audioStart = new Audio("./sound/mc poze.mp3");
 const gameOverSound = new Audio("./sound/audio_gameover.mp3");
 
 let gameStarted = false;
